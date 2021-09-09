41ff7c608d9dd226c3407b94ca269fb8d8b6b687
Ben Vizy <benvizy@gmail.com>
2021-09-09 14:27:05 -0600

    Add home.html

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class User(db.Model):
     country = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
 
-    cart = relationship("Cart", back_populates="userId")
+    cart = relationship("Cart", back_populates="user")
 
     def to_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
@@ -50,7 +50,7 @@ class Product(db.Model):
     description = db.Column(db.String(500), nullable=False)
     image = db.Column(db.String(100), nullable=False)
 
-    cart = relationship("Cart", back_populates="productId")
+    cart = relationship("Cart", back_populates="product")
 
     def to_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
@@ -63,8 +63,60 @@ class Cart(db.Model):
 
     userId = db.Column(db.Integer, db.ForeignKey("user.userId"))
     productId = db.Column(db.Integer, db.ForeignKey("product.productId"))
+    user = relationship("User", back_populates="cart")
+    product = relationship("Product", back_populates="cart")
 
     def to_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 # db.create_all()
+
+# Add some Products
+# product1 = Product(
+#     name="Japanese Tapestry",
+#     price=30,
+#     description="2401 Kanji laid out in a colorful grid for you to memorize",
+#     image='tapestry.png')
+#
+# product2 = Product(
+#     name="New Idaho",
+#     price=20,
+#     description="A novel set 30 years into the future, analyzing the consequences of Augmented Reality",
+#     image='new-idaho.png')
+#
+# product3 = Product(
+#     name="Japanese Pokemon Encyclopedia",
+#     price=50,
+#     description="A Pokemon encyclopedia in Japanese",
+#     image='pokemon.png')
+#
+# db.session.add(product1)
+# db.session.add(product2)
+# db.session.add(product3)
+# db.session.commit()
+
+
+
+# Create a User Login Form
+
+
+
+
+
+
+## Back-End Direction!
+
+@app.route('/')
+def home():
+    products = db.session.query(Product).all()
+    try:
+        print(products[0].name)
+    except:
+        print('product list empty')
+    return render_template('home.html', products=products)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
